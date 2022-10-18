1ebfd683bb2f274d86e0f4426f3119e4415ae5cc
YELLO-belly <112176919+YELLO-belly@users.noreply.github.com>
2022-10-18 03:31:12 +0300

    Update Make.cmd

--- Make.cmd
+++ Make.cmd
@@ -1,3 +1,4 @@
 windres ogg-winmm.rc.in -O coff -o ogg-winmm.rc.o
-gcc -std=gnu99 -Wl,--enable-stdcall-fixup -Ilibs/include -O2 -shared -s -o ogg-winmm.dll ogg-winmm.c player.c stubs.c ogg-winmm.def ogg-winmm.rc.o -L. -lvorbisfile -lwinmm -D_DEBUG -static-libgcc
+gcc -std=gnu99 -Wl,--enable-stdcall-fixup -Ilibs/include -O2 -shared -s -o ogg-winmm.dll ogg-winmm.c player.c stubs.c ogg-winmm.def ogg-winmm.rc.o -L. -l:libvorbisfile.a -l:libvorbis.a -l:libogg.a -lwinmm -D_DEBUG -static
+ren ogg-winmm.dll winmm.dll
 pause
